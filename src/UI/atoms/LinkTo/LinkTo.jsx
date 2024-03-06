71715a8fa3a72a1ce9e6f0df55f20704277964e1
Jesus <sotocodev@gmail.com>
2024-03-06 19:22:50 -0400

    ejemplo

--- src/UI/atoms/LinkTo/LinkTo.jsx
+++ src/UI/atoms/LinkTo/LinkTo.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import styles from './LinkTo.module.css';
 
+
 const LinkTo = ({path, callback, text, customClass}) => {
   return <Link className={`${styles.LinkTo} ${customClass}`} onClick={callback} to={path}>
     {text}
