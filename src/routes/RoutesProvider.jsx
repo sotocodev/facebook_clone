725636fbf895672f2f6a044c6efa0f9db0805494
leonardo <squirrelandrendy@gmail.com>
2024-04-22 12:43:04 -0400

    Error de vercel arreglado

--- src/routes/RoutesProvider.jsx
+++ src/routes/RoutesProvider.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home";
-import MarketplacesPage from "../pages/MarketPlacesPage";
 import MarketPlacePage from "../pages/MarketPlacePage";
 
 const RoutesProvider = () => {
