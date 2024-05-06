73d7e947ca50caebec247786c66509ba5410fd0c
VectorDev27 <nunezbolivarv@gmail.com>
2024-05-06 16:12:48 -0400

    Terminado Zona de Juegos

--- src/UI/atoms/Paragraph/Paragraph.jsx
+++ src/UI/atoms/Paragraph/Paragraph.jsx
@@ -1,7 +1,19 @@
+
+import Icon from '../Icon/Icon';
 import styles from './Paragraph.module.css';
 
-const Paragraph = ({text, customClass}) => {
+const Paragraph = ({text,iconPath, customClass, ParagraphContainerCustomClass, iconCustomClass, IconContainerCustomClass}) => {
   return (
+    
+      iconPath ? (
+        <div className={`${styles.ParagraphContainer} ${ParagraphContainerCustomClass}`}>
+          <div className={IconContainerCustomClass}>
+          <Icon path={iconPath} customClass={iconCustomClass}/>
+          </div>
+          <p className={customClass}>{text}</p>
+        </div>
+      ):
+    
     <p className={`${styles.Paragraph} ${customClass}`}>{text}</p>
   )
 }
--- src/UI/templates/HomeTemplate/HomeTemplate.jsx
+++ src/UI/templates/HomeTemplate/HomeTemplate.jsx
@@ -1,12 +1,16 @@
 import SlidePrincipal from '../../organisms/SlidePrincipal/SlidePrincipal';
 import PublicateZone from '../../organisms/Publicate/PublicateZone';
+import ZonaJuegos from '../../organisms/ZonaJuegos/ZonaJuegos';
+import LeftGameMain from '../../molecules/LeftGameMain/LeftGameMain';
+
 import styles from './HomeTemplate.module.css';
 
 const HomeTemplate = () => {
   return (
     <div className={styles.HomeTemplate}>
-      <SlidePrincipal />
-      <PublicateZone/>
+      <ZonaJuegos/>
+  
+     
     </div>
   )
 }
